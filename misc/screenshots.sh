ab1825b615e8b036f10d6cc38031036ccd43fa8a
Jack Ellis <jackellis1504@gmail.com>
2019-10-14 17:07:10 +0100

    Multiplatform screenshotting

--- misc/screenshots.sh
+++ misc/screenshots.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+if uname | grep -q Darwin; then             # Macs and Linux work differently sometimes
+  chrome="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+else                                        # Whereas...
+  chrome="chromium-browser"
+fi
+
+
 for i in {1..13}
 do
-  chromium-browser --headless\
+  "$chrome" --headless\
     --disable-gpu\
     --screenshot=$i.png\
     --window-size=1024,7680\

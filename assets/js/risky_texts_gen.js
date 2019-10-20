b182a6fbd6e9283a11b47b8b629e97e7dc8d6f1c
Jack Ellis <jackellis1504@gmail.com>
2019-10-20 22:53:09 +0100

    Add the other half of the previous commit

---
+++ assets/js/risky_texts_gen.js
@@ -0,0 +1,65 @@
+function generate_texts() {
+  var allTextsDiv = document.getElementById("all-texts-div");
+  while (allTextsDiv.firstChild) {
+    allTextsDiv.removeChild(allTextsDiv.firstChild);
+  }
+
+
+
+  var texts = document.getElementById("input-form").value.split("\n\n");
+  var lastPerson = "";
+  var firstText = true;
+
+  texts.forEach(function(item) {
+    console.log(item);
+    var regex = /(.*?):([\s\S]*)/s
+    var person = item.split(regex)[1];
+    var message = item.split(regex)[2];
+
+    console.log(person, message);
+
+    var colDiv = document.createElement("div");
+    var conDiv = document.createElement("div");
+    var textDiv = document.createElement("div");
+
+    var textClasses = "text";
+    var colClasses = "col-12";
+    var conClasses = "d-flex";
+
+    if (lastPerson === person) {
+      colClasses += " pt-1";
+    } else {
+      colClasses += " pt-3";
+      textClasses += " chain-top";
+    }
+
+    switch (person) {
+      case "me":
+        textClasses += " me";
+        conClasses += " flex-row-reverse";
+        break;
+      case "them":
+        textClasses += " them";
+        conClasses += " flex-row";
+        break;
+      case "time":
+        textClasses += " time text-center";
+        break;
+      default:
+        conClasses = "d-none";
+        break;
+    }
+
+    colDiv.className = colClasses;
+    conDiv.className = conClasses;
+    textDiv.className = textClasses;
+    textDiv.innerHTML = message.replace("\n","</br>");
+
+    conDiv.appendChild(textDiv);
+    colDiv.appendChild(conDiv);
+    allTextsDiv.appendChild(colDiv);
+
+
+    lastPerson = person;
+  });
+}

0e60335b5f578a74f288f06e13bd57220bc2c5b2
Jack Ellis <jackellis1504@gmail.com>
2021-04-03 13:47:34 +0100

    Rearrangement and commenting

--- _assets/js/app.js
+++ _assets/js/app.js
@@ -1,10 +1,6 @@
-var toggle = document.querySelector("#nav-toggle");
-var nav_list = document.querySelectorAll(".navbar__list")[0];
-
-toggle.onclick = function() {
-  toggle.classList.toggle("active");
-  nav_list.classList.toggle("active");
-}
+/**
+ * Vue scaffolding
+ */
 
 import Vue from 'vue';
 import season_gen from './vue_components/SeasonGen.vue';
@@ -21,3 +17,15 @@ const app = new Vue({
     permissions
   },
 });
+
+/**
+ * Nav menu stuff
+ */
+
+var toggle = document.querySelector("#nav-toggle");
+var nav_list = document.querySelectorAll(".navbar__list")[0];
+
+toggle.onclick = function() {
+  toggle.classList.toggle("active");
+  nav_list.classList.toggle("active");
+}

73d5f9d2d278d1ca2d76202024340a8a43808102
Jack Ellis <jackellis1504@gmail.com>
2022-01-04 21:26:45 +0000

    Update the head

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "nuxt-2",
+    title: "Jack Ellis",
     htmlAttrs: {
       lang: "en",
     },

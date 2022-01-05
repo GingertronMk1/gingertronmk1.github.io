50d10807e5df6b295c2572bb9e173efe138d457c
Jack Ellis <jackellis1504@gmail.com>
2022-01-05 10:27:22 +0000

    Added a footer

---
+++ components/FooterComponent.vue
@@ -0,0 +1,99 @@
+<template>
+  <footer class="footer">
+    <div class="footer__column footer__socials">
+      <span v-for="(social, index) in socials" :key="`social-${index}`">
+        <i :class="social.class" />
+        <a :href="social.url" target="_blank" v-text="social.name" />
+      </span>
+    </div>
+    <div class="footer__column">
+      <div class="footer__credit">
+        This website is built using Nuxt and hosted on GitHub Pages. To see the
+        source code click
+        <a
+          href="https://github.com/gingertronmk1/gingertronmk1.github.io"
+          target="_blank"
+          v-text="`here`"
+        />
+      </div>
+    </div>
+    <div class="footer__column">
+      &copy; Jack Ellis {{ new Date().getFullYear() }}
+    </div>
+  </footer>
+</template>
+<script>
+export default {
+  props: {},
+  data() {
+    return {
+      socials: [
+        {
+          name: "Twitter",
+          url: "https://www.twitter.com/GingertronMk1",
+          class: "fab fa-twitter",
+        },
+        {
+          name: "GitHub",
+          url: "https://www.github.com/GingertronMk1",
+          class: "fab fa-github",
+        },
+        {
+          name: "LinkedIn",
+          url: "https://www.linkedin.com/in/jackellis1504",
+          class: "fab fa-linkedin",
+        },
+      ],
+    };
+  },
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style lang="scss">
+.footer {
+  @include flex(row, space-between, stretch);
+  background-color: $grey-2;
+  color: $white;
+  margin-top: 1rem;
+
+  * {
+    color: inherit;
+  }
+
+  &__column {
+    @include flex(column, flex-end, stretch);
+    flex: 1;
+    min-width: 1px;
+    padding: 1rem;
+    text-align: center;
+
+    &:first-of-type {
+      text-align: left;
+    }
+
+    &:last-of-type {
+      text-align: right;
+    }
+
+    > * {
+      text-align: inherit;
+    }
+  }
+
+  &__socials {
+    a {
+      text-decoration: none;
+    }
+
+    span + span {
+      margin-top: 0.5rem;
+    }
+  }
+
+  &__credit {
+    font-size: 0.7rem;
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,8 @@
   <div class="body">
     <NavBar />
     <Nuxt class="body__inner" :class="routePath" />
+    <div class="flex-filler"></div>
+    <FooterComponent />
   </div>
 </template>
 <script>
@@ -21,3 +23,27 @@ export default {
   methods: {},
 };
 </script>
+
+<style lang="scss">
+.body {
+  @include flex(column, space-between, stretch);
+
+  &__inner {
+    margin: $nav-margin auto 0;
+    width: 95%;
+    max-width: $large;
+
+    // Cheeky special case for the index page
+    &.index {
+      margin: $nav-margin 0 0;
+      width: 100%;
+      max-width: unset;
+    }
+  }
+
+  .flex-filler {
+    flex: 1;
+    min-height: 1px;
+  }
+}
+</style>

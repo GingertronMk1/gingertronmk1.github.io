a9774e9dd0a206c562e2f29a549e68539a0fc41f
Jack Ellis <jackellis1504@gmail.com>
2022-01-06 20:40:38 +0000

    Added a sitemap

---
+++ pages/sitemap.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="sitemap">
+    <table>
+      <thead>
+        <tr>
+          <th>Path</th>
+          <th>Name</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(route, index) in routes" :key="`route-${index}`">
+          <td v-text="route.path" />
+          <td v-text="route.name" />
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<script>
+export default {
+  name: "SitemapPage",
+  computed: {
+    routes() {
+      const ret = this.$router.options.routes;
+      return ret.sort((a, b) => {
+        if (a.path < b.path) {
+          return -1;
+        } else if (a.path > b.path) {
+          return 1;
+        }
+        return 0;
+      });
+    },
+  },
+  mounted() {
+    window.router = this.$router;
+  },
+};
+</script>
+
+<style lang="scss">
+.sitemap table {
+  margin: 0 auto;
+  border-spacing: 0;
+
+  th,
+  td {
+    padding: 0.5rem;
+  }
+
+  th {
+    border-bottom: 2px solid $grey-3;
+  }
+
+  tr:nth-child(2n) {
+    background-color: $grey-0;
+  }
+}
+</style>

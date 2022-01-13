dd6afa1c431d9a8c6a2f9b278f9e3e10feadbc2b
Jack Ellis <jackellis1504@gmail.com>
2022-01-13 17:28:16 +0000

    Added a bunch of problems

---
+++ components/euler/10.vue
@@ -0,0 +1,38 @@
+<template>
+  <section>
+    <h3 v-text="result(input)" />
+    <input v-model="input" type="number" />
+    <pre v-text="$options.methods.result" />
+  </section>
+</template>
+<script>
+export default {
+  name: "ProblemTen",
+  props: {},
+  data() {
+    return {
+      input: 2000000,
+    };
+  },
+  methods: {
+    result(limit) {
+      const ret = [];
+      const isPrime = (number) => {
+        for (let i = 2; i <= Math.sqrt(number); i++) {
+          if (number % i === 0) {
+            return false;
+          }
+        }
+        return true;
+      };
+
+      for (let i = 2; i < limit; i++) {
+        if (isPrime(i)) {
+          ret.push(i);
+        }
+      }
+      return ret.reduce((a, b) => a + b, 0);
+    },
+  },
+};
+</script>
---
+++ components/euler/7.vue
@@ -0,0 +1,40 @@
+<template>
+  <section>
+    <h3 v-text="result(input)" />
+    <input v-model="input" type="number" />
+    <pre v-text="$options.methods.result" />
+  </section>
+</template>
+<script>
+export default {
+  name: "ProblemSeven",
+  props: {},
+  data() {
+    return {
+      input: 10001,
+    };
+  },
+  methods: {
+    result(limit) {
+      let ret = 0;
+      let retCount = 0;
+      const isPrime = (number) => {
+        for (let i = 2; i <= Math.sqrt(number); i++) {
+          if (number % i === 0) {
+            return false;
+          }
+        }
+        return true;
+      };
+
+      for (let i = 2; retCount < limit; i++) {
+        if (isPrime(i)) {
+          ret = i;
+          retCount++;
+        }
+      }
+      return ret;
+    },
+  },
+};
+</script>

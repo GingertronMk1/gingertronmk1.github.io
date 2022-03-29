a363698d868f8c6b51b45ff20d973c686d13b1fd
Jack Ellis <jackellis1504@gmail.com>
2022-03-29 18:09:52 +0100

    Modified navlinks

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,39 @@
+<script setup>
+const { ref } = require("@vue/reactivity");
+
+const navLinks = ref([
+  {
+    text: "Home",
+    to: "/",
+  },
+  {
+    text: "RoS",
+    to: "/vue_utilities/redditorOfSteel",
+  },
+]);
+</script>
 <template>
-  <nav class="navbar navbar-dark bg-primary">
-    <router-link to="/">Home</router-link>
-    <i class="fas fa-chevron-down" />
-    <router-link to="/about">About</router-link>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container-fluid">
+      <router-link class="navbar-brand" to="/">
+        <i class="fas fa-home" />
+      </router-link>
+      <ul class="navbar-nav">
+        <li
+          v-for="(link, index) in navLinks"
+          :key="`navLink${index}`"
+          class="nav-item"
+        >
+          <router-link :to="link.to" class="nav-link">
+            {{ link.text }}
+          </router-link>
+        </li>
+      </ul>
+    </div>
   </nav>
-  <router-view />
+  <main class="container-fluid">
+    <router-view />
+  </main>
 </template>
 
 <style lang="scss">

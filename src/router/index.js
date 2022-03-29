a9b92a58cde4b15063c58e172363a17b8bf27892
Jack Ellis <jackellis1504@gmail.com>
2022-03-29 20:43:26 +0100

    Once more

--- src/router/index.js
+++ src/router/index.js
@@ -18,9 +18,10 @@ pages.keys().forEach((page) => {
 
 routes.push({
   name: "404",
-  path: "/:catchAll(.*)",
+  path: "/:pathMatch(.*)",
   component: NotFound,
 });
+
 
 process.env.NODE_ENV !== "production" && console.table(routes);
 

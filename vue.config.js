b9b68d53a0b0a8e27a0e5b4f1e2725c6f65ade52
Jack Ellis <jackellis1504@gmail.com>
2022-03-29 18:08:37 +0100

    Added sourcemaps

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   css: {
+    sourceMap: process.env.NODE_ENV === "development",
     loaderOptions: {
       sass: {
         additionalData: '@import "@/scss/resources/resources.scss";',

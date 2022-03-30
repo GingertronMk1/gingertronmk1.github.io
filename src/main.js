e96dde8dd95f98c212c6fe12314add0ad3575afe
Jack Ellis <jackellis1504@gmail.com>
2022-03-30 13:10:48 +0100

    Moved bits around

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
+/**
+ * Vue setup
+ */
+
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-require("bootstrap");
 
 createApp(App).use(router).mount("#app");
+
+/**
+ * Bootstrap setup
+ */
+
+require("bootstrap");

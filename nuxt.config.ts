9ee66574e87032ecfda7e0fe7138d3f7eee2b50d
Jack Ellis <jackellis1504@gmail.com>
2024-06-11 11:30:02 +0100

    dockerised
     M nuxt.config.ts
     M package-lock.json
    ?? docker-compose.yml

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/content']
+  modules: ['@nuxt/content'],
+  devServer: {
+    host: "0.0.0.0",
+    port: 80,
+  },
+  vite: {
+    server: {
+      hmr: {
+        port: 23456,
+      },
+    },
+  },
 })

4b43fe4211732a0ad5ce5a52a27bf66f240afbe1
Jack Ellis <jackellis1504@gmail.com>
2024-06-11 13:43:40 +0100

    make my name clickable
     M components/AppNav.vue

--- components/AppNav.vue
+++ components/AppNav.vue
@@ -3,7 +3,7 @@
 <template>
     <nav class="site-nav">
         <div class="site-nav__inner">
-            <h1 class="site-nav__header-text">Jack Ellis</h1>
+            <NuxtLink to="/"><h1 class="site-nav__header-text">Jack Ellis</h1></NuxtLink>
         <div class="site-nav__nav-links">
             <NuxtLink to="/cv">CV</NuxtLink>
         </div>

e05effcb4835ffd4978ec606aafaaecfbf058745
Jack Ellis <jackellis1504@gmail.com>
2025-02-17 17:51:18 +0000

    update without stupid textarea
     M pages/times.vue

--- pages/times.vue
+++ pages/times.vue
@@ -1,33 +1,133 @@
 <script setup lang="ts">
 
 import {computed} from "vue";
-import moment from "moment-timezone";
+import moment from "moment";
+import momentTz from "moment-timezone";
 
-const inputStrings = ref<string>('');
+interface Time {
+  hours: number;
+  minutes: number;
+  day: number;
+  month: number;
+  year: number;
+  timezone: string;
+}
 
-const convertedTimes = computed<moment.Moment[]>(function() {
-  const individualTimes: string[] = inputStrings.value.split('\n');
-  return individualTimes.map(function (individualTime: string) {
-    const [time, timeZone] = individualTime
-        .split('|')
-        .map((timePart: string) => timePart.trim());
-    const newTime = moment.tz(time, timeZone);
-    const newTimeHere = newTime.clone().tz('Europe/London')
-    return newTimeHere;
-  })
-});
+const inputTimes = ref<Array<Time>>([]);
+
+const addTime = function () {
+  const now = moment();
+  inputTimes.value.push({
+    hours: now.hour(),
+    minutes: now.minute(),
+    day: now.date(),
+    month: now.month() + 1,
+    year: now.year(),
+    timezone: now.zoneName(),
+  });
+}
+
+const removeTime = function (t: Time) {
+  inputTimes.value = inputTimes.value.filter((_t: Time) => _t !== t);
+}
+
+addTime();
+
+const convertTime = function (t: Time) {
+  const newTime = momentTz.tz({
+    ...t,
+    month: t.month - 1,
+    seconds: 0
+  }, t.timezone);
+  return newTime.clone().tz('Europe/London')
+};
 </script>
 
 <template>
 
-  <textarea name="times" id="times" cols="30" rows="10" v-model="inputStrings" />
+  <div class="times">
+    <div v-for="(time, index) in inputTimes" :key="index" class="times__input-time">
+      <label class="times__input-time-part" for="hours">
+        Hours
+        <input type="number" v-model="time.hours" id="hours"/>
+      </label>
+      <label class="times__input-time-part" for="minutes">
+        Minutes
+        <input type="number" v-model="time.minutes" id="minutes"/>
+      </label>
+      <label class="times__input-time-part" for="year">
+        Year
+        <input type="number" v-model="time.year" id="year"/>
+      </label>
+      <label class="times__input-time-part" for="month">
+        Month
+        <input type="number" v-model="time.month" id="month"/>
+      </label>
+      <label class="times__input-time-part" for="day">
+        Day
+        <input type="number" v-model="time.day" id="day"/>
+      </label>
+      <label for="timezone" class="times__input-time-part times__input-time-part--timezone">
+        Timezone
+        <input type="text" v-model="time.timezone" id="timezone"/>
+      </label>
+      <div class="times__time-display">
+        <span v-text="convertTime(time)"/>
+        <button @click="removeTime(time)">Remove</button>
+      </div>
+    </div>
+  </div>
 
-  <ul>
-    <li v-for="(time, index) in convertedTimes" :key="index" v-text="time.toLocaleString()" />
-  </ul>
+  <button @click="addTime">Add Time</button>
 
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+.times {
+  display: flex;
+  flex-direction: column;
+
+  &__time-display {
+    display: flex;
+    min-width: 100%;
+    flex: 1;
+    margin-top: 0.5rem;
+
+    & > button {
+      margin-inline-start: auto;
+    }
+  }
+
+  &__input-time {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 1rem 0;
+
+    & + & {
+      border-top: 2px solid black;
+    }
+  }
+
+  &__input-time-part {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    min-width: 1%;
+
+    & + &:not(&--timezone) {
+      margin-inline-start: 0.25rem;
+    }
+
+    &--timezone {
+      min-width: 100%;
+    }
+  }
+
+  pre {
+    min-width: 100%;
+    flex: 1;
+    margin: 0;
+  }
+}
 
 </style>

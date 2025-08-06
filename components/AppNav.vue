dbbfc38fd961e0dbc831d7b4b855f94bbef6b92f
Jack Ellis <jackellis1504@gmail.com>
2025-08-06 22:08:44 +0100

    formatting
     M components/AppNav.vue

--- components/AppNav.vue
+++ components/AppNav.vue
@@ -18,7 +18,11 @@ const links = ref([
 <template>
   <nav class="p-4 bg-orange-600 text-white text-2xl text-semibold">
     <div class=" flex flex-row justify-between">
-      <NuxtLink to="/"><h1 class="site-nav__header-text">Jack Ellis</h1></NuxtLink>
+      <NuxtLink to="/" class="hover:text-gray-300">
+        <h1 class="site-nav__header-text">
+          Jack Ellis
+        </h1>
+      </NuxtLink>
       <div class="space-x-4">
         <NuxtLink
             class="hover:text-gray-300"

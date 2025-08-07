7c5910bb36ac1044beff0c76a24bbd2f10957e88
Jack Ellis <jackellis1504@gmail.com>
2025-08-07 15:12:06 +0100

    Component updates

---
+++ components/AppButton.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="p-4 bg-orange-600 rounded-xl text-white text-2xl font-bold cursor-pointer">
+    <slot />
+  </div>
+</template>
---
+++ components/AppInput.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+withDefaults(defineProps<{
+  type: 'number' | 'text' | 'textarea' | 'checkbox' | 'radio'
+}>(), {
+  type: 'text'
+});
+
+const model = defineModel<string|number|boolean>();
+</script>
+
+<template>
+  <input :type="type" v-model="model" class="p-4 border-2 border-gray-300 rounded-md"/>
+</template>

0070f0d42b34247925ed9288cd8582109689e8a9
Jack Ellis <jackellis1504@gmail.com>
2025-08-18 14:51:37 +0100

    wip
     M components/ContainerComponent.vue
    ?? bin/fix

--- components/ContainerComponent.vue
+++ components/ContainerComponent.vue
@@ -1,6 +1,4 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div class="container mx-auto w-[95%]">
@@ -8,6 +6,4 @@
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>

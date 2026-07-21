1aa346a970bf90091d19e83449c56a0fbea2d96a
Jack Ellis <jackellis1504@gmail.com>
2026-07-21 08:25:10 +0100

    not hash
     M src/router/index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 import CounterIndex from "@/pages/counter/CounterIndex.vue";
 import IndexIndex from "@/pages/index/IndexIndex.vue";
@@ -6,7 +6,7 @@ import RFLTranslatorIndex from "@/pages/rfl-translator/RFLTranslatorIndex.vue";
 import MoveIndex from "@/pages/move/MoveIndex.vue";
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       name: "counter",
